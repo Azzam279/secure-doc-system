b4f990fb7d36cb0d5cf72c436ec68ee213f46636
Azzam279 <azzam2579@gmail.com>
2026-03-12 16:01:55 +0800

    fix: document server-action

--- apps/frontend/actions/document.ts
+++ apps/frontend/actions/document.ts
@@ -1,13 +1,23 @@
 "use server";
 
 import { apiFetch } from "@/lib/api";
+import { cookies } from "next/headers";
 
 export async function requestSignedUrl(file: File) {
+  const cookie = await cookies()
+  const sessionCookie = cookie.get("session");
+  if (!sessionCookie) {
+    throw new Error("No session");
+  }
+
   const { url, storagePath } = await apiFetch<{
     url: string;
     storagePath: string;
   }>("/documents/upload-url", {
     method: "POST",
+    headers: {
+      cookie: `${sessionCookie.name}=${sessionCookie.value}`,
+    },
     body: JSON.stringify({
       fileName: file.name,
       mimeType: file.type,
@@ -20,8 +30,17 @@ export async function requestSignedUrl(file: File) {
 }
 
 export async function saveMetadata(storagePath: string, file: File) {
+  const cookie = await cookies()
+  const sessionCookie = cookie.get("session");
+  if (!sessionCookie) {
+    throw new Error("No session");
+  }
+
   await apiFetch("/documents", {
     method: "POST",
+    headers: {
+      cookie: `${sessionCookie.name}=${sessionCookie.value}`,
+    },
     body: JSON.stringify({
       storagePath,
       fileName: file.name,
